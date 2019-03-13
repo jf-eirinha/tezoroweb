4296f364c6709c67a00d44dd0846b28130a494d5
Joao Eirinha <joaoeirinh@gmail.com>
2019-03-13 14:26:39 +0000

    added a navbar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
+import Topbar from './components/Topbar';
 
 class App extends Component {
   render() {
     return (
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center">
-        <Grid item>
-          <Grid container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            spacing={40}>
-              <Grid item>1</Grid>
-              <Grid item>2</Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Grid container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            spacing={40}>
-              <Grid item>3</Grid>
-              <Grid item>4</Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+      // <Grid
+      //   container
+      //   direction="column"
+      //   justify="center"
+      //   alignItems="center">
+
+      //   <Grid container>
+           <Topbar></Topbar>
+      //   </Grid>
+
+      //   <Grid container
+      //     direction="row"
+      //     justify="space-around"
+      //     alignItems="center">
+      //       <Grid item>Hello 1</Grid>
+      //       <Grid item>Hello 2</Grid>
+      //   </Grid>
+
+      //   <Grid container
+      //     direction="row"
+      //     justify="space-around"
+      //     alignItems="center">
+      //       <Grid item>Hello 3</Grid>
+      //       <Grid item>Hello 4</Grid>
+      //   </Grid>
+
+      // </Grid>
     );
   }
 }
