6839ff5819865952ed5cfe049ca92692407e6b97
Joao Eirinha <joaoeirinh@gmail.com>
2019-03-14 03:56:08 +0000

    customization. added logo

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default class Topbar extends React.Component {
 
     render() {
         return (
             <Navbar bg="white" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img
+                        src={require('../logo.svg')}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="Tezoro logo"
+                    />
+                    {' Tezoro '}
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav>
